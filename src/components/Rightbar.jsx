3a77fc047821eaff738202e16e87b7e60f8f55a2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-28 01:54:13 -0400

    Added birthday to rightbar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,9 +1,17 @@
 import '../css/Rightbar.css';
+import gift from '../assets/gift.png';
 
 function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src={gift} alt="" />
+                    <span className="birthdayText">
+                        <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
