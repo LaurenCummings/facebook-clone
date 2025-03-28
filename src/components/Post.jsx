b2e9310037881ecd951fba00ba82e8f0ad1f072b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-28 01:37:56 -0400

    Added content to post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,8 @@
 import '../css/Post.css';
 import person1 from '../assets/person/1.jpeg';
+import post1 from '../assets/post/1.jpeg';
+import like from '../assets/like.png';
+import heart from '../assets/heart.png';
 import { MoreVert } from '@mui/icons-material';
 
 function Post() {
@@ -17,10 +20,18 @@ function Post() {
                     </div>
                 </div>
                 <div className="postCenter">
-
+                    <span className="postText">Hey! It's my first post</span>
+                    <img className="postImg" src={post1} alt="" />
                 </div>
                 <div className="postBottom">
-
+                    <div className="postBottomLeft">
+                        <img className="likeIcon" src={like} alt="" />
+                        <img className="likeIcon" src={heart} alt="" />
+                        <span className="postLikeCounter">32 people like it</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">9 comments</span>
+                    </div>
                 </div>
             </div>
         </div>
