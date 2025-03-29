91731575301099d2d1bf8a00c40b1ec849f7ef73
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-29 00:43:39 -0400

    Incorporated dummy data into post - had to change image location so server could access

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,20 +1,16 @@
 import '../css/Feed.css';
 import Share from '../components/Share';
 import Post from '../components/Post';
+import { Posts } from '../dummyData.js';
 
 function Feed() {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {Posts.map((p) => (
+                    <Post key={p.id} post={p} />
+                ))}
             </div>
         </div>
     )
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,7 @@ import like from '../assets/like.png';
 import heart from '../assets/heart.png';
 import { MoreVert } from '@mui/icons-material';
 
-function Post() {
+function Post({post}) {
     return (
         <div className="post">
             <div className="postWrapper">
@@ -13,24 +13,24 @@ function Post() {
                     <div className="postTopLeft">
                         <img className="postProfileImg" src={person1} alt="" />
                         <span className="postUsername">Safak Kocaoglu</span>
-                        <span className="postDate">5 mins ago</span>
+                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Hey! It's my first post</span>
-                    <img className="postImg" src={post1} alt="" />
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImg" src={post.photo} alt="" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img className="likeIcon" src={like} alt="" />
                         <img className="likeIcon" src={heart} alt="" />
-                        <span className="postLikeCounter">32 people like it</span>
+                        <span className="postLikeCounter">{post.like}</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">9 comments</span>
+                        <span className="postCommentText">{post.comment}</span>
                     </div>
                 </div>
             </div>
