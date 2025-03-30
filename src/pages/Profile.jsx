dc0478ab4a1f901e581475c7224df0748b7ecf4c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-30 01:50:33 -0400

    Added content to Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,25 @@
 import '../css/Profile.css';
+import Topbar from '../components/Topbar';
+import Leftbar from '../components/Leftbar';
+import Feed from '../components/Feed';
+import Rightbar from '../components/Rightbar';
 
 function Profile() {
     return (
         <div>
-            hey
+            <Topbar />
+            <div className="profile">
+                <Leftbar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <Rightbar />                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
