15b416cda43f185c1571807c4d08b04cf173ad9b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-30 02:10:19 -0400

    Added content and styling to Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,8 @@ import Topbar from '../components/Topbar';
 import Leftbar from '../components/Leftbar';
 import Feed from '../components/Feed';
 import Rightbar from '../components/Rightbar';
+import coverImage from '../assets/post/3.jpeg';
+import userImage from '../assets/person/7.jpeg';
 
 function Profile() {
     return (
@@ -12,7 +14,22 @@ function Profile() {
                 <Leftbar />
                 <div className="profileRight">
                     <div className="profileRightTop">
-
+                        <div className="profileCover">
+                            <img 
+                                className="profileCoverImg" 
+                                src={coverImage} 
+                                alt="" 
+                            />
+                            <img 
+                                className="profileUserImg" 
+                                src={userImage} 
+                                alt="" 
+                            />                            
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                            <span className="profileInfoDesc">Hello my friends</span>
+                        </div>
                     </div>
                     <div className="profileRightBottom">
                         <Feed />
