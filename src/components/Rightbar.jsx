d962d0524ea92f9fcf4b0510fcf055674e805514
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-30 02:34:19 -0400

    Added right bar followings to profile

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,9 +1,14 @@
 import '../css/Rightbar.css';
 import gift from '../assets/gift.png';
 import ad from '../assets/ad.png';
-import person3 from '../assets/person/3.jpeg';
 import { Users } from '../dummyData';
 import Online from '../components/Online';
+import person1 from '../assets/person/1.jpeg';
+import person2 from '../assets/person/2.jpeg';
+import person3 from '../assets/person/3.jpeg';
+import person4 from '../assets/person/4.jpeg';
+import person5 from '../assets/person/5.jpeg';
+import person6 from '../assets/person/6.jpeg';
 
 function Rightbar({profile}) {
 
@@ -43,6 +48,33 @@ function Rightbar({profile}) {
                     <div className="rightbarInfoItem">
                         <span className="rightbarInfoKey">Relationship:</span>
                         <span className="rightbarInfoValue">Single</span>
+                    </div>
+                </div>
+                <h4 className="rightbarTitle">User Friends</h4>
+                <div className="rightbarFollowings">
+                    <div className="rightbarFollowing">
+                        <img className="rightbarFollowingImg" src={person1} alt="" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img className="rightbarFollowingImg" src={person2} alt="" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img className="rightbarFollowingImg" src={person3} alt="" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img className="rightbarFollowingImg" src={person4} alt="" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img className="rightbarFollowingImg" src={person5} alt="" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img className="rightbarFollowingImg" src={person6} alt="" />
+                        <span className="rightbarFollowingName">John Carter</span>
                     </div>
                 </div>
             </>
