b94245162e91169b0960d219d8a307dc6592a493
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-31 00:27:07 -0400

    Added logic for rightbar (home or profile)

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <Register />
+      <Home />
     </div>
   )
 }
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -84,7 +84,7 @@ function Rightbar({profile}) {
     return (
         <div className="rightbar">
             <div className="rightbarWrapper">
-                <ProfileRightbar />
+                {profile ? <ProfileRightbar /> : <HomeRightbar /> }
             </div>
         </div>
     )
