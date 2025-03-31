65159d5535ab36198f5b3788576a63e7a714ae25
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-30 23:34:15 -0400

    Added structure to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,20 @@ import '../css/Login.css';
 
 function Login() {
     return (
-        <div>
-
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Lamasocial</h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world around you on Lamasocial.
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <div className="loginBox">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
