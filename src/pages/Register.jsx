b7e33e11f11915debe46618fd6ab970cafebe9e1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-31 00:39:43 -0400

    changed class names for register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,22 +2,22 @@ import '../css/Register.css';
 
 function Register() {
     return (
-        <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">Lamasocial</h3>
-                    <span className="loginDesc">
+        <div className="register">
+            <div className="registerWrapper">
+                <div className="registerLeft">
+                    <h3 className="registerLogo">Lamasocial</h3>
+                    <span className="registerDesc">
                         Connect with friends and the world around you on Lamasocial.
                     </span>
                 </div>
-                <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Username" className="loginInput" />
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
-                        <input placeholder="Password Again" className="loginInput" />
-                        <button className="loginButton">Sign Up</button>
-                        <button className="loginRegisterButton">
+                <div className="registerRight">
+                    <div className="registerBox">
+                        <input placeholder="Username" className="registerInput" />
+                        <input placeholder="Email" className="registerInput" />
+                        <input placeholder="Password" className="registerInput" />
+                        <input placeholder="Password Again" className="registerInput" />
+                        <button className="registerRegisterButton">Sign Up</button>
+                        <button className="registerLoginButton">
                             Log into Account
                         </button>
                     </div>
